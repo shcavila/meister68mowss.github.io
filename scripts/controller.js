dafba3de4b4cadcefbc009d93bd32ca8880bfae9
meister68 <53040397+meister68@users.noreply.github.com>
2019-10-09 14:13:24 +0800

    Update controller.js

--- scripts/controller.js
+++ scripts/controller.js
@@ -14,14 +14,6 @@ $(document).ready(function () {
    
 
 
-    var currentdate = new Date(); 
-    var datetime = "Now: " + currentdate.getDate() + "/"
-    + (currentdate.getMonth()+1)  + "/" 
-    + currentdate.getFullYear() + " @ "  
-    + currentdate.getHours() + ":"  
-    + currentdate.getMinutes() + ":" 
-    + currentdate.getSeconds();
-
     $('#btnOn').click(function () {
         let topic = 'sharmen/device/status';
         let payload = 'Turned On '+new Date($.now())
